0c40a5321cec168fd5e467a1b1e219039dacfa60
Claudiu Belu <cbelu@cloudbasesolutions.com>
2015-08-08 01:45:42 +0300

    Adds jobutils module
    Adds jobutils module which contains checking return values
    from WMI jobs, waiting for WMI jobs and WMI jobs such as:
    - add / modify / remove virtual resources
    - add / remove virtual features

---
+++ oslo_windows/tests/utils/test_jobutils.py
@@ -0,0 +1,177 @@
+#  Copyright 2015 Cloudbase Solutions Srl
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+from oslotest import base
+
+from oslo_windows import exceptions
+from oslo_windows.utils import constants
+from oslo_windows.utils import jobutils
+
+
+class JobUtilsTestCase(base.BaseTestCase):
+    """Unit tests for the Hyper-V JobUtils class."""
+
+    _FAKE_RET_VAL = 0
+
+    _FAKE_JOB_STATUS_BAD = -1
+    _FAKE_JOB_DESCRIPTION = "fake_job_description"
+    _FAKE_JOB_PATH = 'fake_job_path'
+    _FAKE_ERROR = "fake_error"
+    _FAKE_ELAPSED_TIME = 0
+    _CONCRETE_JOB = "Msvm_ConcreteJob"
+
+    def setUp(self):
+        super(JobUtilsTestCase, self).setUp()
+        self.jobutils = jobutils.JobUtils()
+        self.jobutils._conn = mock.MagicMock()
+
+    @mock.patch.object(jobutils.JobUtils, '_wait_for_job')
+    def test_check_ret_val_started(self, mock_wait_for_job):
+        self.jobutils.check_ret_val(constants.WMI_JOB_STATUS_STARTED,
+                                    mock.sentinel.job_path)
+        mock_wait_for_job.assert_called_once_with(mock.sentinel.job_path)
+
+    @mock.patch.object(jobutils.JobUtils, '_wait_for_job')
+    def test_check_ret_val_ok(self, mock_wait_for_job):
+        self.jobutils.check_ret_val(self._FAKE_RET_VAL,
+                                    mock.sentinel.job_path)
+        self.assertFalse(mock_wait_for_job.called)
+
+    def test_check_ret_val_exception(self):
+        self.assertRaises(exceptions.HyperVException,
+                          self.jobutils.check_ret_val,
+                          mock.sentinel.ret_val_bad,
+                          mock.sentinel.job_path)
+
+    def test_wait_for_job_exception_concrete_job(self):
+        mock_job = self._prepare_wait_for_job()
+        mock_job.path.return_value.Class = self._CONCRETE_JOB
+        self.assertRaises(exceptions.HyperVException,
+                          self.jobutils._wait_for_job,
+                          self._FAKE_JOB_PATH)
+
+    def test_wait_for_job_exception_with_error(self):
+        mock_job = self._prepare_wait_for_job()
+        mock_job.GetError.return_value = (self._FAKE_ERROR, self._FAKE_RET_VAL)
+        self.assertRaises(exceptions.HyperVException,
+                          self.jobutils._wait_for_job,
+                          self._FAKE_JOB_PATH)
+        mock_job.GetError.assert_called_once_with()
+
+    def test_wait_for_job_exception_no_error_details(self):
+        mock_job = self._prepare_wait_for_job()
+        mock_job.GetError.return_value = (None, None)
+        self.assertRaises(exceptions.HyperVException,
+                          self.jobutils._wait_for_job,
+                          self._FAKE_JOB_PATH)
+
+    def test_wait_for_job_ok(self):
+        mock_job = self._prepare_wait_for_job(
+            constants.WMI_JOB_STATE_COMPLETED)
+        job = self.jobutils._wait_for_job(self._FAKE_JOB_PATH)
+        self.assertEqual(mock_job, job)
+
+    def _prepare_wait_for_job(self, state=_FAKE_JOB_STATUS_BAD):
+        mock_job = mock.MagicMock()
+        mock_job.JobState = state
+        mock_job.Description = self._FAKE_JOB_DESCRIPTION
+        mock_job.ElapsedTime = self._FAKE_ELAPSED_TIME
+
+        jobutils.wmi.WMI = mock.MagicMock(return_value=mock_job)
+        return mock_job
+
+    def test_add_virt_resource(self):
+        self._test_virt_method('AddVirtualSystemResources', 3,
+                               'add_virt_resource', True,
+                               [mock.sentinel.res_data], mock.sentinel.vm_path)
+
+    def test_modify_virt_resource(self):
+        self._test_virt_method('ModifyVirtualSystemResources', 2,
+                               'modify_virt_resource', True,
+                               ResourceSettingData=[mock.sentinel.res_data],
+                               ComputerSystem=mock.sentinel.vm_path)
+
+    def test_remove_virt_resource(self):
+        self._test_virt_method('RemoveVirtualSystemResources', 2,
+                               'remove_virt_resource', True,
+                               [mock.sentinel.res_path], mock.sentinel.vm_path)
+
+    def _test_virt_method(self, vsms_method_name, return_count,
+                          utils_method_name, with_mock_vm, *args, **kwargs):
+        mock_svc = self.jobutils._conn.Msvm_VirtualSystemManagementService()[0]
+        vsms_method = getattr(mock_svc, vsms_method_name)
+        mock_rsd = self._mock_vsms_method(vsms_method, return_count)
+        if with_mock_vm:
+            mock_vm = mock.MagicMock()
+            mock_vm.path_.return_value = mock.sentinel.vm_path
+            getattr(self.jobutils, utils_method_name)(mock_rsd, mock_vm)
+        else:
+            getattr(self.jobutils, utils_method_name)(mock_rsd)
+
+        if args:
+            vsms_method.assert_called_once_with(*args)
+        else:
+            vsms_method.assert_called_once_with(**kwargs)
+
+    def _mock_vsms_method(self, vsms_method, return_count):
+        args = None
+        if return_count == 3:
+            args = (
+                mock.sentinel.job_path, mock.MagicMock(), self._FAKE_RET_VAL)
+        else:
+            args = (mock.sentinel.job_path, self._FAKE_RET_VAL)
+
+        vsms_method.return_value = args
+        mock_res_setting_data = mock.MagicMock()
+        mock_res_setting_data.GetText_.return_value = mock.sentinel.res_data
+        mock_res_setting_data.path_.return_value = mock.sentinel.res_path
+
+        self.jobutils.check_ret_val = mock.MagicMock()
+
+        return mock_res_setting_data
+
+
+class JobUtilsV2TestCase(JobUtilsTestCase):
+    """Unit tests for the Hyper-V JobUtils class."""
+
+    def setUp(self):
+        super(JobUtilsV2TestCase, self).setUp()
+        self.jobutils = jobutils.JobUtilsV2()
+        self.jobutils._conn = mock.MagicMock()
+
+    def test_add_virt_resource(self):
+        self._test_virt_method('AddResourceSettings', 3, 'add_virt_resource',
+                               True, mock.sentinel.vm_path,
+                               [mock.sentinel.res_data])
+
+    def test_modify_virt_resource(self):
+        self._test_virt_method('ModifyResourceSettings', 3,
+                               'modify_virt_resource', True,
+                               ResourceSettings=[mock.sentinel.res_data])
+
+    def test_remove_virt_resource(self):
+        self._test_virt_method('RemoveResourceSettings', 2,
+                               'remove_virt_resource', True,
+                               ResourceSettings=[mock.sentinel.res_path])
+
+    def test_add_virt_feature(self):
+        self._test_virt_method('AddFeatureSettings', 3, 'add_virt_feature',
+                               True, mock.sentinel.vm_path,
+                               [mock.sentinel.res_data])
+
+    def test_remove_virt_feature(self):
+        self._test_virt_method('RemoveFeatureSettings', 2,
+                               'remove_virt_feature', False,
+                               FeatureSettings=[mock.sentinel.res_path])
---
+++ oslo_windows/utils/jobutils.py
@@ -0,0 +1,163 @@
+# Copyright 2015 Cloudbase Solutions Srl
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+Base Utility class for operations on Hyper-V.
+"""
+
+import sys
+import time
+
+if sys.platform == 'win32':
+    import wmi
+
+from oslo_log import log as logging
+
+from oslo_windows._i18n import _
+from oslo_windows import exceptions
+from oslo_windows.utils import constants
+
+LOG = logging.getLogger(__name__)
+
+
+class JobUtils(object):
+
+    _WMI_NAMESPACE = '//%s/root/virtualization'
+
+    def __init__(self, host='.'):
+        if sys.platform == 'win32':
+            self._init_hyperv_wmi_conn(host)
+
+    def _init_hyperv_wmi_conn(self, host):
+        self._conn = wmi.WMI(moniker=self._WMI_NAMESPACE % host)
+
+    def check_ret_val(self, ret_val, job_path, success_values=[0]):
+        if ret_val in [constants.WMI_JOB_STATUS_STARTED,
+                       constants.WMI_JOB_STATE_RUNNING]:
+            return self._wait_for_job(job_path)
+        elif ret_val not in success_values:
+            raise exceptions.HyperVException(
+                _('Operation failed with return value: %s') % ret_val)
+
+    def _wait_for_job(self, job_path):
+        """Poll WMI job state and wait for completion."""
+
+        job_wmi_path = job_path.replace('\\', '/')
+        job = wmi.WMI(moniker=job_wmi_path)
+
+        while job.JobState == constants.WMI_JOB_STATE_RUNNING:
+            time.sleep(0.1)
+            job = wmi.WMI(moniker=job_wmi_path)
+        if job.JobState != constants.WMI_JOB_STATE_COMPLETED:
+            job_state = job.JobState
+            if job.path().Class == "Msvm_ConcreteJob":
+                err_sum_desc = job.ErrorSummaryDescription
+                err_desc = job.ErrorDescription
+                err_code = job.ErrorCode
+                data = {'job_state': job_state,
+                        'err_sum_desc': err_sum_desc,
+                        'err_desc': err_desc,
+                        'err_code': err_code}
+                raise exceptions.HyperVException(
+                    _("WMI job failed with status %(job_state)d. "
+                      "Error details: %(err_sum_desc)s - %(err_desc)s - "
+                      "Error code: %(err_code)d") % data)
+            else:
+                (error, ret_val) = job.GetError()
+                if not ret_val and error:
+                    data = {'job_state': job_state,
+                            'error': error}
+                    raise exceptions.HyperVException(
+                        _("WMI job failed with status %(job_state)d. "
+                          "Error details: %(error)s") % data)
+                else:
+                    raise exceptions.HyperVException(
+                        _("WMI job failed with status %d. No error "
+                          "description available") % job_state)
+        desc = job.Description
+        elap = job.ElapsedTime
+        LOG.debug("WMI job succeeded: %(desc)s, Elapsed=%(elap)s",
+                  {'desc': desc, 'elap': elap})
+        return job
+
+    def add_virt_resource(self, virt_resource, parent):
+        """Adds a new resource to the VM."""
+        vs_man_svc = self._conn.Msvm_VirtualSystemManagementService()[0]
+        res_xml = [virt_resource.GetText_(1)]
+        (job_path,
+         new_resources,
+         ret_val) = vs_man_svc.AddVirtualSystemResources(res_xml,
+                                                         parent.path_())
+        self.check_ret_val(ret_val, job_path)
+        return new_resources
+
+    def modify_virt_resource(self, virt_resource, parent):
+        """Updates a VM resource."""
+        vs_man_svc = self._conn.Msvm_VirtualSystemManagementService()[0]
+        (job_path, ret_val) = vs_man_svc.ModifyVirtualSystemResources(
+            ResourceSettingData=[virt_resource.GetText_(1)],
+            ComputerSystem=parent.path_())
+        self.check_ret_val(ret_val, job_path)
+
+    def remove_virt_resource(self, virt_resource, parent):
+        """Removes a VM resource."""
+        vs_man_svc = self._conn.Msvm_VirtualSystemManagementService()[0]
+        res_path = [virt_resource.path_()]
+        (job_path, ret_val) = vs_man_svc.RemoveVirtualSystemResources(
+            res_path, parent.path_())
+        self.check_ret_val(ret_val, job_path)
+
+
+class JobUtilsV2(JobUtils):
+
+    _WMI_NAMESPACE = '//%s/root/virtualization/v2'
+
+    def add_virt_resource(self, virt_resource, parent):
+        vs_man_svc = self._conn.Msvm_VirtualSystemManagementService()[0]
+        (job_path, new_resources, ret_val) = vs_man_svc.AddResourceSettings(
+            parent.path_(), [virt_resource.GetText_(1)])
+        self.check_ret_val(ret_val, job_path)
+        return new_resources
+
+    def modify_virt_resource(self, virt_resource, parent=None):
+        vs_man_svc = self._conn.Msvm_VirtualSystemManagementService()[0]
+        (job_path, out_set_data, ret_val) = vs_man_svc.ModifyResourceSettings(
+            ResourceSettings=[virt_resource.GetText_(1)])
+        self.check_ret_val(ret_val, job_path)
+
+    def remove_virt_resource(self, virt_resource, parent=None):
+        vs_man_svc = self._conn.Msvm_VirtualSystemManagementService()[0]
+        (job, ret_val) = vs_man_svc.RemoveResourceSettings(
+            ResourceSettings=[virt_resource.path_()])
+        self.check_ret_val(ret_val, job)
+
+    def add_virt_feature(self, virt_feature, parent):
+        self.add_multiple_virt_features([virt_feature], parent)
+
+    def add_multiple_virt_features(self, virt_features, parent):
+        vs_man_svc = self._conn.Msvm_VirtualSystemManagementService()[0]
+        (job_path, out_set_data, ret_val) = vs_man_svc.AddFeatureSettings(
+            parent.path_(), [f.GetText_(1) for f in virt_features])
+        self.check_ret_val(ret_val, job_path)
+
+    def remove_virt_feature(self, virt_feature):
+        self.remove_multiple_virt_features([virt_feature])
+
+    def remove_multiple_virt_features(self, virt_features):
+        vs_man_svc = self._conn.Msvm_VirtualSystemManagementService()[0]
+        (job_path, ret_val) = vs_man_svc.RemoveFeatureSettings(
+            FeatureSettings=[f.path_() for f in virt_features])
+        self.check_ret_val(ret_val, job_path)
