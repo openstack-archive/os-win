542292ee46cc50db2bf58e02d73dfcb9262fc0e0
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2016-05-10 12:53:56 +0300

    Add method for retrieving local share paths
    This will be used by Nova in order to properly handle volumes
    residing on local shares.
    
    Partial-Bug: 1580122
    Change-Id: I8e4426b3b1044d24563adf7826ab9f141c2495b8
    (cherry picked from commit a0fd9cee3734173a20cc3ef32e741336dff71d9d)

--- os_win/utils/storage/smbutils.py
+++ os_win/utils/storage/smbutils.py
@@ -112,3 +112,10 @@ class SMBUtils(baseutils.BaseUtils):
                 return 0, 0
             else:
                 raise exc
+
+    def get_smb_share_path(self, share_name):
+        shares = self._smb_conn.Msft_SmbShare(Name=share_name)
+        share_path = shares[0].Path if shares else None
+        if not shares:
+            LOG.debug("Could not find any local share named %s.", share_name)
+        return share_path
