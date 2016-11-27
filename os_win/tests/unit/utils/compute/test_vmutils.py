f31e91f2f2736dddfc1171b8202b26346b37566d
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2016-11-27 17:17:35 +0200

    Improve VM snapshots support
    Adds a parameter to optionally name a snapshot and a method
    to enumerate existing VM snapshots.
    
    Co-Authored-By: Lucian Petrut <lpetrut@cloudbasesolutions.com>
    
    Change-Id: Ifc1b6ae02a914b13121fcd7e5f1f7c0d6911edb7
    Partially-Implements: blueprint hyperv-assisted-volume-snapshot

--- os_win/tests/unit/utils/compute/test_vmutils.py
+++ os_win/tests/unit/utils/compute/test_vmutils.py
@@ -628,26 +628,65 @@ class VMUtilsTestCase(test_base.OsWinBaseTestCase):
             mock.sentinel.fake_new_mounted_disk_path,
             mock_rasds[0].HostResource[0])
 
-    def test_take_vm_snapshot(self):
-        mock_vm = self._lookup_vm()
-        mock_snap = mock.MagicMock(Antecedent=mock_vm)
-        self._vmutils._conn.Msvm_MostCurrentSnapshotInBranch.return_value = [
-            mock.MagicMock(), mock_snap]
+    @mock.patch.object(vmutils.VMUtils, '_modify_virtual_system')
+    @ddt.data(None, mock.sentinel.snap_name)
+    def test_take_vm_snapshot(self, snap_name, mock_modify_virtual_system):
+        self._lookup_vm()
+        mock_snap = mock.Mock(ElementName=mock.sentinel.default_snap_name)
 
         mock_svc = self._get_snapshot_service()
         mock_svc.CreateSnapshot.return_value = (self._FAKE_JOB_PATH,
                                                 mock.MagicMock(),
                                                 self._FAKE_RET_VAL)
 
-        snap_path = self._vmutils.take_vm_snapshot(self._FAKE_VM_NAME)
+        mock_job = self._vmutils._jobutils.check_ret_val.return_value
+        mock_job.associators.return_value = [mock_snap]
 
-        self.assertEqual(mock_snap.Dependent.path_.return_value, snap_path)
+        snap_path = self._vmutils.take_vm_snapshot(self._FAKE_VM_NAME,
+                                                   snap_name)
+
+        self.assertEqual(mock_snap.path_.return_value, snap_path)
         mock_svc.CreateSnapshot.assert_called_with(
             AffectedSystem=self._FAKE_VM_PATH,
             SnapshotType=self._vmutils._SNAPSHOT_FULL)
 
         self._vmutils._jobutils.check_ret_val.assert_called_once_with(
             self._FAKE_RET_VAL, self._FAKE_JOB_PATH)
+
+        mock_job.associators.assert_called_once_with(
+            wmi_result_class=self._vmutils._VIRTUAL_SYSTEM_SETTING_DATA_CLASS,
+            wmi_association_class=self._vmutils._AFFECTED_JOB_ELEMENT_CLASS)
+
+        if snap_name:
+            self.assertEqual(snap_name, mock_snap.ElementName)
+            mock_modify_virtual_system.assert_called_once_with(mock_snap)
+        else:
+            self.assertEqual(mock.sentinel.default_snap_name,
+                             mock_snap.ElementName)
+            mock_modify_virtual_system.assert_not_called()
+
+    @ddt.data(None, mock.sentinel.snap1)
+    def test_get_vm_snapshots(self, snap_name):
+        mock_snap1 = mock.Mock(ElementName=mock.sentinel.snap1)
+        mock_snap2 = mock.Mock(ElementName=mock.sentinel.snap2)
+
+        mock_vm = self._lookup_vm()
+        mock_vm.associators.return_value = [mock_snap1, mock_snap2]
+
+        snaps = self._vmutils.get_vm_snapshots(mock.sentinel.vm_name,
+                                               snap_name)
+
+        expected_snaps = [mock_snap1.path_.return_value]
+        if not snap_name:
+            expected_snaps += [mock_snap2.path_.return_value]
+
+        self.assertEqual(expected_snaps, snaps)
+
+        mock_vm.associators.assert_called_once_with(
+            wmi_association_class=(
+                self._vmutils._VIRTUAL_SYSTEM_SNAP_ASSOC_CLASS),
+            wmi_result_class=(
+                self._vmutils._VIRTUAL_SYSTEM_SETTING_DATA_CLASS))
 
     def test_remove_vm_snapshot(self):
         mock_svc = self._get_snapshot_service()
