a3f39a5593f4fbc10dcdb0fd30ad0d36ddab5146
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2017-02-10 19:00:27 +0200

    Synchronize iscsi_target_utils
    The iSCSI target server WMI methods often deadlock when run in parallel.
    
    We experienced those issues after starting executing WMI calls in
    parallel. On our CI, we've seen cases where about 20 VHD creation
    requests hang for about 20 minutes, consuming PyMI's thread pool and
    effectively blocking Cinder.
    
    Synchronizing the according WMI calls seems to be the only workaround
    we have at the moment.
    
    Closes-Bug: #1664270
    
    Change-Id: I08b8c81a56f80a9664007a7f67fcab1689cd6549

--- os_win/utils/baseutils.py
+++ os_win/utils/baseutils.py
@@ -20,9 +20,16 @@ Base WMI utility class.
 
 import imp
 import sys
+import threading
+import time
 
 if sys.platform == 'win32':
     import wmi
+
+from oslo_log import log as logging
+from oslo_utils import reflection
+
+LOG = logging.getLogger(__name__)
 
 
 class BaseUtils(object):
@@ -105,3 +112,40 @@ class BaseUtilsVirt(BaseUtils):
         if not compatibility_mode or BaseUtilsVirt._os_version >= [6, 3]:
             return wmi.WMI(moniker=moniker, **kwargs)
         return self._get_wmi_compat_conn(moniker=moniker, **kwargs)
+
+
+class SynchronizedMeta(type):
+    """Use an rlock to synchronize all class methods."""
+
+    def __init__(cls, cls_name, bases, attrs):
+        super(SynchronizedMeta, cls).__init__(cls_name, bases, attrs)
+        rlock = threading.RLock()
+
+        for attr_name in attrs:
+            attr = getattr(cls, attr_name)
+            if callable(attr):
+                decorated = SynchronizedMeta._synchronize(
+                    attr, cls_name, rlock)
+                setattr(cls, attr_name, decorated)
+
+    @staticmethod
+    def _synchronize(func, cls_name, rlock):
+        def wrapper(*args, **kwargs):
+            f_qual_name = reflection.get_callable_name(func)
+
+            t_request = time.time()
+            try:
+                with rlock:
+                    t_acquire = time.time()
+                    LOG.debug("Method %(method_name)s acquired rlock. "
+                              "Waited %(time_wait)0.3fs",
+                              dict(method_name=f_qual_name,
+                                   time_wait=t_acquire - t_request))
+                    return func(*args, **kwargs)
+            finally:
+                t_release = time.time()
+                LOG.debug("Method %(method_name)s released rlock. "
+                          "Held %(time_held)0.3fs",
+                          dict(method_name=f_qual_name,
+                               time_held=t_release - t_acquire))
+        return wrapper
